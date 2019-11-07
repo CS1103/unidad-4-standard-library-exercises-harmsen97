1cdb238ef565bf96ed1a95af162df09ff5c93be6
Jeronimo Harmsen <54420156+harmsen97@users.noreply.github.com>
2019-11-07 18:24:01 -0500

    Add files via upload

---
+++ Open_source/cmake-build-debug/CMakeFiles/Open_source.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Open_source.dir/main.cpp.o"
+  "Open_source.pdb"
+  "Open_source"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Open_source.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Open_source/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+
+
+int main() {
+
+
+    std::string line;
+    std::map<std::string, std::string> users;
+    std::string os, username;
+    std::unordered_map<std::string, int> freq;
+
+    while(true){
+
+        std::getline(std::cin, line);
+
+        if(line[0] == '1') {
+            break;
+        }
+
+        if(isupper(line[0])){
+            os = line;
+            freq[os] = 0;
+        }
+        else{
+            auto it = users.find(line);
+            if(it != users.end()) {
+                if(it->second != os) {
+                    users.erase(it);
+                }
+            }
+            else {
+                users[line] = os;
+            }
+        }
+    }
+
+    for(auto&i:users)
+        freq[i.second]++;
+
+    std::vector<std::pair<std::string,int>> freqs;
+
+    for(auto&i:freq)
+        freqs.push_back(i);
+
+    std::sort(freqs.begin(), freqs.end(), comp);
+
+    for(auto&i:freqs)
+        std::cout<<i.first<<' '<<i.second<<'\n';
+
+    return 0;
+
+
+}
